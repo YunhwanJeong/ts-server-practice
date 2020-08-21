3794b7b06333f4e895cd0d6eea84a6f0461f0d62
aiden-jung <jimmy53120488@gmail.com>
2020-08-21 15:06:36 +0900

    CRUD: Read, Update, Delete complete

--- src/resolvers/BookResolver.ts
+++ src/resolvers/BookResolver.ts
@@ -1,8 +1,16 @@
-import { Resolver, Arg, Int, Mutation, InputType, Field } from "type-graphql";
+import {
+  Resolver,
+  Arg,
+  Int,
+  Mutation,
+  InputType,
+  Field,
+  Query,
+} from "type-graphql";
 import { Book } from "../entity/Book";
 
 @InputType()
-class BookInputType {
+class BookCreateInput {
   @Field()
   title: string;
 
@@ -10,10 +18,37 @@ class BookInputType {
   price: number;
 }
 
+@InputType()
+class BookUpdateInput {
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field(() => Int, { nullable: true })
+  price?: number;
+}
+
 @Resolver(Book)
 export class BookResolver {
   @Mutation(() => Book)
-  async addBook(@Arg("options") options: BookInputType) {
+  async createBook(@Arg("options") options: BookCreateInput) {
     return await Book.create(options).save();
+  }
+  @Mutation(() => Boolean)
+  async updateBook(
+    @Arg("id", () => Int) id: number,
+    @Arg("data") data: BookUpdateInput
+  ) {
+    await Book.update(id, data);
+    return true;
+  }
+  @Mutation(() => Boolean)
+  async deleteBook(@Arg("id") id: number) {
+    await Book.delete(id);
+    return true;
+  }
+
+  @Query(() => [Book])
+  async getBooks() {
+    return await Book.find();
   }
 }
